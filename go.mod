fa1e2853ea4b16826b81e676a632f27d530a922b
Marcus Crane <marcus@utf9k.net>
2022-12-12 10:27:57 +1300

    Remove unused structs plus wire up span context, not that it is used

--- go.mod
+++ go.mod
@@ -5,18 +5,15 @@ go 1.19
 require (
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 	go.opentelemetry.io/otel v1.11.2
-	go.opentelemetry.io/otel/sdk v1.11.2
-	go.opentelemetry.io/otel/trace v1.11.2
 	go.opentelemetry.io/proto/otlp v0.19.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
-	github.com/go-logr/logr v1.2.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.14.0 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opentelemetry.io/otel/trace v1.11.2 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
--- types.go
+++ types.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/sdk/instrumentation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/trace"
-	"time"
-)
-
 /// Datadog
 
 // spanContext contains a bunch of state that may or may not
@@ -46,6 +36,8 @@ type ddspan struct {
 	TraceID  uint64             `msgpack:"trace_id"`
 	ParentID uint64             `msgpack:"parent_id"`
 	Error    int32              `msgpack:"error"`
+
+	Context *spanContext `msgpack:"-"`
 }
 
 // trace is a group of spans that share the same trace ID.
@@ -61,30 +53,3 @@ type ddtrace []ddspan
 // Opendog does not deal with more than one trace at a time
 // but msgpack traces come in a bundle we still deserialise
 type ddtraces []ddtrace
-
-/// OpenTelemetry
-
-type status struct {
-	code        codes.Code
-	description string
-}
-
-// See https://github.com/open-telemetry/opentelemetry-go/blob/4e763472eee5ef03b97274694d2d1b07f0b3cd11/sdk/trace/span.go#L110
-type otelspan struct {
-	Name                 string
-	SpanContext          trace.SpanContext
-	Parent               trace.SpanContext
-	SpanKind             trace.SpanKind
-	StartTime            time.Time
-	EndTime              time.Time
-	Attributes           []attribute.KeyValue
-	Events               []tracesdk.Event
-	Links                []tracesdk.Link
-	Status               tracesdk.Status
-	DroppedAttributes    int
-	DroppedEvents        int
-	DroppedLinks         int
-	ChildSpanCount       int
-	Resource             *resource.Resource
-	InstrumentationScope instrumentation.Scope
-}
